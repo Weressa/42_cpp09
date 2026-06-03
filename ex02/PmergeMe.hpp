a08b1824158ef079dac401295b432cbdafba84a5
assabich <assabich@student.42.fr>
2026-06-03 21:15:02 +0200

    ford sort

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: assabich <assabich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/18 20:19:13 by assabich          #+#    #+#             */
-/*   Updated: 2026/06/02 20:19:58 by assabich         ###   ########.fr       */
+/*   Updated: 2026/06/03 21:09:09 by assabich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,31 @@
 #include <climits>
 #include <cstdlib>
 
+struct Pair
+{
+    int big;
+    int small;
+};
+
+template <typename T>
+typename T::iterator    insert_position(T &cont, int value)
+{
+    typename T::iterator    left = cont.begin();
+    typename T::iterator    right = cont.end();
+    
+    while (left < right)
+    {
+        typename T::iterator mid = left + (right - left) / 2;
+        
+        if (*mid < value)
+            left = mid + 1;
+        else
+            right = mid;
+    }
+    return (left);
+}
+
+
 class PmergeMe
 {
     public:
@@ -27,7 +52,9 @@ class PmergeMe
     PmergeMe(const PmergeMe &copy);
     ~PmergeMe();
     PmergeMe &operator=(const PmergeMe &copy);
-    void    print_result();
+    
+    void    print_before();
+    void    sort();
 
     private:
         PmergeMe();
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: assabich <assabich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/18 20:19:42 by assabich          #+#    #+#             */
-/*   Updated: 2026/06/02 20:18:46 by assabich         ###   ########.fr       */
+/*   Updated: 2026/06/03 20:59:01 by assabich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ int main(int ac, char** av)
         if (ac < 2)
             throw std::runtime_error("Error: format must be:\n./PmergeMe nb1 nb2 ... (minimum two integers)\n");
         PmergeMe    merge(ac, av);        
-        merge.print_result();
+        merge.print_before();
+        merge.sort();
     }
     catch (const std::exception &e)
     {
